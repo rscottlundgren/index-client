e1a74286d186b5452d3a1ec9afa29af2fa8ab1a2
rscottlundgren <rscottlundgren@gmail.com>
2020-04-02 18:42:46 -0400

    Set-Up 'Sign-Up' Curl-Script - 1.  Repurposed 'Sign-Up' curl-script from prior tic-tac-toe project for     duplicative use in project two.

---
+++ curl-scripts/auth/sign-up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "http://critical-index.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
