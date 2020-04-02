c2f52792a7a4b9cf3e5b9a2228782f6d5234f35c
rscottlundgren <rscottlundgren@gmail.com>
2020-04-02 18:37:31 -0400

    Set-Up Events Functions - 1.  Repurposed events functions from prior tic-tac-toe project to build     a shell. 2.  Required functions/data from the "config.js", "store.js", and     "get-form-fields.js" file.

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const getFormFields = require('../../../lib/get-form-fields')
+const api = require('./api')
+const ui = require('./ui')
+
+// Sign-Up Event
+const onSignUp = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+// Sign-In Event
+const onSignIn = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+// Change Password Event
+const onChangePassword = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+// Sign-Out Event
+const onSignOut = function (event) {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
