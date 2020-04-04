97b15e67b9af34da7e12229b0d8c28dda634e8a3
rscottlundgren <rscottlundgren@gmail.com>
2020-04-03 20:46:23 -0400

    Update Click Handlers - 1) Add new click handler for "create-character" form. 2) Add new click handler for "update-character" form. 3) Add new click handler for "delete-character" form. 4) Add new click handler for "show-all" form. 5) Add new click handler for "single-character" form.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@
 // const example = require('./example')
 
 const authEvents = require('./auth/events')
+const charEvents = require('./character_engine/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
@@ -13,4 +14,9 @@ $(() => {
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('submit', authEvents.onSignOut)
+  $('#create-character').on('submit', charEvents.onCreateCharacter)
+  $('#update-character').on('submit', charEvents.onUpdateCharacter)
+  $('#delete-character').on('submit', charEvents.onDeleteCharacter)
+  $('#show-all').on('submit', charEvents.onShowAll)
+  $('#single-character').on('submit', charEvents.onShowOne)
 })
