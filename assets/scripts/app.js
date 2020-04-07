19dab940629731addfd973ef95d21eb20d3671b2
rscottlundgren <rscottlundgren@gmail.com>
2020-04-07 16:24:12 -0400

    Update For Handlebars & Comments - 1) Add comments for all Click Events. 2) Updated "Create Character" click event for use with Handlebars. 3) Updated "Update Character" click event for use with Handlebars. 4) Updated "Register/Sign-Up" click event for use with Handlebars. 5) Updated "Sign-In" click event for use with Handlebars.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,17 +12,60 @@ const charUI = require('./character_engine/ui.js')
 // require('./example')
 
 $(() => {
-  $('#sign-up').on('submit', authEvents.onSignUp)
-  $('#sign-in').on('submit', authEvents.onSignIn)
+  // Click Events ------------------------------------------------------------->
+
+  // Authorization - Events --------------------------------------------------->
+  // Register/Sign-Up --------------------------------------------------------->
+  $('#blurb-form-row').on('submit', '#sign-up', authEvents.onSignUp)
+
+  // Sign-In ------------------------------------------------------------------>
+  $('#blurb-form-row').on('submit', '#sign-in', authEvents.onSignIn)
+
+  // Change Password ---------------------------------------------------------->
   $('#delta-pw-form').on('submit', authEvents.onChangePassword)
+
+  // Sign-Out ----------------------------------------------------------------->
   $('#sign-out').on('click', authEvents.onSignOut)
-  $('#create-character').on('submit', charEvents.onCreateCharacter)
-  $('#update-character').on('submit', charEvents.onUpdateCharacter)
-  $('#char-list-accord').on('click', '#del-btn',charEvents.onDeleteCharacter)
-  $('#show-all').on('click', charEvents.onShowAll)
-  $('#show-one-form').on('submit', charEvents.onShowOne)
+
+  // Authorization - UI ------------------------------------------------------->
+  // Show Registration/Sign-Up Drop Down Menu --------------------------------->
   $('#reg-dd').on('click', authUI.onDropDownRegistration)
+
+  // Show Sign-In Drop Down Menu ---------------------------------------------->
   $('#si-dd').on('click', authUI.onDropDownSignIn)
+
+  // Show Change Password Inline Form ----------------------------------------->
   $('#delta-pw').on('click', authUI.onChangePasswordReveal)
-  $("#show-one").on('click', charUI.onShowOneCharacter)
+
+  // Character - Events ------------------------------------------------------->
+  // Create New Character ----------------------------------------------------->
+  $('#generate-update').on('submit', '#create-character', charEvents.onCreateCharacter)
+
+  // Update Character --------------------------------------------------------->
+  $('#generate-update').on('submit', '#update-character', charEvents.onUpdateCharacter)
+
+  // Delete Character --------------------------------------------------------->
+  $('#char-list-accord').on('click', '#del-btn',charEvents.onDeleteCharacter)
+
+  // Show All Characters ------------------------------------------------------>
+  $('#show-all').on('click', charEvents.onShowAll)
+
+  // Show One Character ------------------------------------------------------->
+  $('#show-one-form').on('submit', charEvents.onShowOne)
+
+  // Character - UI ----------------------------------------------------------->
+  // Show Create Character Form ----------------------------------------------->
+  $('#show-create-form').on('click', charUI.onShowCharacterGenerator)
+
+  // Show Update Character Form ----------------------------------------------->
+  $('#show-update-form').on('click', charUI.onShowUpdateCharacter)
+
+  // Show One Character Inline Form ------------------------------------------->
+  $('#show-one').on('click', charUI.onShowOneCharacter)
+
+  // Show About Page ---------------------------------------------------------->
+  $('#about-lp').on('click', charUI.onShowAboutPage)
+
+  // Show Home Page ----------------------------------------------------------->
+  $('#home-lp').on('click', charUI.onShowHomePage)
 })
