01a7b6eb70964eb64ab01634f01867a0b2ee3981
rscottlundgren <rscottlundgren@gmail.com>
2020-04-07 17:43:24 -0400

    Add Comments & Clearer Variable Names - 1) Added comments for organization/future understanding. 2) Renamed "data" variables as "userInput" for better understanding.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -4,40 +4,42 @@ const getFormFields = require('../../../lib/get-form-fields')
 const api = require('./api')
 const ui = require('./ui')
 
-// Sign-Up Event
+// Sign-Up Event -------------------------------------------------------------->
 const onSignUp = function (event) {
   event.preventDefault()
-  const data = getFormFields(event.target)
-  api.signUp(data)
+  const userInput = getFormFields(event.target)
+  console.log(userInput)
+  api.signUp(userInput)
     .then(ui.signUpSuccess)
     .catch(ui.signUpFailure)
 }
 
-// Sign-In Event
+// Sign-In Event -------------------------------------------------------------->
 const onSignIn = function (event) {
   event.preventDefault()
-  const data = getFormFields(event.target)
-  api.signIn(data)
+  const userInput = getFormFields(event.target)
+  api.signIn(userInput)
     .then(ui.signInSuccess)
     .catch(ui.signInFailure)
 }
 
-// Change Password Event
+// Change Password Event ------------------------------------------------------>
 const onChangePassword = function (event) {
   event.preventDefault()
-  const data = getFormFields(event.target)
-  api.changePassword(data)
+  const userInput = getFormFields(event.target)
+  api.changePassword(userInput)
     .then(ui.changePasswordSuccess)
     .catch(ui.changePasswordFailure)
 }
 
-// Sign-Out Event
+// Sign-Out Event ------------------------------------------------------------->
 const onSignOut = function (event) {
   event.preventDefault()
   api.signOut()
     .then(ui.signOutSuccess)
     .catch(ui.signOutFailure)
 }
+
 module.exports = {
   onSignUp,
   onSignIn,
