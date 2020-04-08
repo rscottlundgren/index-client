8c253f34d2275eb6d1e0d4181fc32a1bafa418d2
rscottlundgren <rscottlundgren@gmail.com>
2020-04-08 08:34:30 -0400

    Removed Last Slash (I'm Silly & Included One)

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://critical-index.herokuapp.com/',
+  production: 'https://critical-index.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
